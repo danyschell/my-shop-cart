4fa8a24613ec4c24842f62343aaabfef91d108a2
Daniel Schell <schell.daniel@gmail.com>
2024-04-10 10:42:41 -0300

    styles.scss

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -7,11 +7,16 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
+  title = 'my-shop-cart';
+  toggleTheme(){
+    if(document.body.classList.contains('dark-theme')) {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+    }
+  }
 }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
